4a458e739ed916d3c29757e262c99c3f4214f599
Eric C. Newton <ecn@apache.org>
2013-06-06 17:12:35 +0000

    ACCUMULO-118 add apache license header to new files
    git-svn-id: https://svn.apache.org/repos/asf/accumulo/branches/ACCUMULO-118@1490362 13f79535-47bb-0310-9956-ffa450edef68

--- server/src/main/java/org/apache/accumulo/server/fs/FileSystemImpl.java
+++ server/src/main/java/org/apache/accumulo/server/fs/FileSystemImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.accumulo.server.fs;
 
 import java.io.FileNotFoundException;
@@ -32,14 +48,6 @@ import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.util.Progressable;
 import org.apache.log4j.Logger;
-
-// hide the need to make blocksize be an even number of checksum blocks 
-//
-//      int checkSum = fs.getConf().getInt("io.bytes.per.checksum", 512);
-//      blockSize -= blockSize % checkSum;
-//      blockSize = Math.max(blockSize, checkSum);
-// if buffersize is zero, get it from the filesystem
-// 
 
 public class FileSystemImpl implements org.apache.accumulo.server.fs.FileSystem {
   
